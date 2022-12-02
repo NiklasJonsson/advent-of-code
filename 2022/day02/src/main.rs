f01570f3acf996abcebbb73df710eaeae94fad08
Niklas Jonsson <niklasandersjonsson@gmail.com>
2022-12-02 06:49:54 +0100

    2022 day 2

---
+++ 2022/day02/src/main.rs
@@ -0,0 +1,91 @@
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+enum GameResult {
+    Win,
+    Draw,
+    Loss,
+}
+
+impl GameResult {
+    fn parse(sym: &str) -> Self {
+        match sym {
+            "X" => Self::Loss,
+            "Y" => Self::Draw,
+            "Z" => Self::Win,
+            _ => panic!("Invalid symbol {sym}")
+        }
+    }
+
+    fn score(&self) -> u32 {
+        match self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Loss => 0,
+        }
+    }
+}
+
+impl Shape {
+    fn parse(sym: &str) -> Self {
+        match sym {
+            "A" => Self::Rock,
+            "B" => Self::Paper,
+            "C" => Self::Scissors,
+            _ => panic!("Invalid symbol {sym}")
+        }
+    }
+
+    fn score(&self) -> u32 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+}
+
+fn parse_score(line: &str) -> u32 {
+    let mut split = line.split_ascii_whitespace();
+    let theirs = Shape::parse(split.next().expect("Missing first action"));
+    let result = GameResult::parse(split.next().expect("Missing first action"));
+    let mine = match (result, theirs) {
+        (GameResult::Draw, _) => theirs,
+        (GameResult::Win, Shape::Paper) => Shape::Scissors,
+        (GameResult::Win, Shape::Rock) => Shape::Paper,
+        (GameResult::Win, Shape::Scissors) => Shape::Rock,
+        (GameResult::Loss, Shape::Paper) => Shape::Rock,
+        (GameResult::Loss, Shape::Rock) => Shape::Scissors,
+        (GameResult::Loss, Shape::Scissors) => Shape::Paper,
+    };
+
+    result.score() + mine.score()
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        let name = &args[0];
+        println!("usage: {name} <file>");
+        return Err("Error: expected file arg".into());
+    }
+
+    let contents = std::fs::read_to_string(&args[1])?;
+    let mut total_score = 0;
+
+    for line in contents.lines().map(str::trim) {
+        if line.is_empty() {
+            continue;
+        }
+
+        total_score += parse_score(line);
+    }
+
+    println!("Score: {total_score}");
+
+    Ok(())
+}
--- 2022/newday.nu
+++ 2022/newday.nu
@@ -3,20 +3,16 @@ def day_dirname [n: int] {
     echo $"day($number)"
 }
 
-let n = (ls day* | sort-by name | get name | last 1 | path basename | str substring '3,' | str to-int)
+let n = (ls day* | sort-by name | get name | last 1 | path basename | str substring '3,' | into int | get 0)
 
 let prev_dir = (day_dirname $n | path expand)
 let next_dir = (day_dirname ($n + 1))
 
-if ($next_dir | path exists) {
-    echo $"($next_dir) already exists, can't create (char nl)"
-} {
-    let new_mem_list = (open Cargo.toml | get workspace.members | append ($next_dir))
-    open Cargo.toml | update workspace.members ($new_mem_list) | save Cargo.toml
-    cargo new --bin ($next_dir)
-    cp $"($prev_dir)/src/main.rs" $"($next_dir)/src/main.rs" 
-    echo "" | save $"($next_dir)/input.txt"
-    echo "" | save $"($next_dir)/simpleinput.txt"
-    git add Cargo.toml
-    git add $next_dir
-}
+let new_mem_list = (open Cargo.toml | get workspace.members | append ($next_dir))
+open Cargo.toml | update workspace.members ($new_mem_list) | save Cargo.toml
+cargo new --bin ($next_dir)
+cp $"($prev_dir)/src/main.rs" $"($next_dir)/src/main.rs"
+echo "" | save $"($next_dir)/input.txt"
+echo "" | save $"($next_dir)/simpleinput.txt"
+git add Cargo.toml
+git add $next_dir
