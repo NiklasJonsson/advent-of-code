9fe34924c7fad2ba6eeef37e9ef18058db35631f
Niklas Jonsson <niklasandersjonsson@gmail.com>
2022-12-03 07:05:07 +0100

    day 3, part 2

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,14 +1,16 @@
+#![feature(iter_next_chunk)]
+
 fn prio(c: char) -> u32 {
-    if c >= 'a' && c <= 'z' {
+    if ('a'..='z').contains(&c) {
         return c as u32 - 'a' as u32 + 1;
     }
 
 
-    if c >= 'A' && c <= 'Z' {
+    if ('A'..='Z').contains(&c) {
         return c as u32 - 'A' as u32 + 27;
     }
 
-    unimplemented!("{}", c);
+    panic!("Invalid: {}", c);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,25 +22,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let contents = std::fs::read_to_string(&args[1])?;
+    assert!(contents.lines().count() % 3 == 0);
     let mut sum = 0;
 
-    for line in contents.lines().map(str::trim) {
-        if line.is_empty() {
-            continue;
-        }
-
-        let len = line.len();
-        assert!(len % 2 == 0);
-        let first: &str = &line[..len/2];
-        let second: &str = &line[len/2..];
-        let first: std::collections::HashSet<char> = std::collections::HashSet::from_iter(first.chars());
-        for f in first {
-            if second.contains(f) {
-                sum += prio(f);
-            }
-        }
-
+    let mut itr = contents.lines().map(str::trim).filter(|s| !s.is_empty());
+    while let Ok(chunk) = itr.next_chunk::<3>() {
+        let first: std::collections::HashSet<char> = std::collections::HashSet::from_iter(chunk[0].chars());
+        let p: Option<u32> = first
+        .iter()
+        .find_map(|&c| (chunk[1].contains(c) && chunk[2].contains(c)).then(|| prio(c)));
+        sum += p.expect("No common item in group");
     }
+
 
     println!("Answer: {sum}");
 
