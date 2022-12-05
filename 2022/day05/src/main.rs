8b5b15145ab7eea8bf01e7bb8817126adac50672
Niklas Jonsson <niklasandersjonsson@gmail.com>
2022-12-05 07:30:03 +0100

    day 5

---
+++ 2022/day05/src/main.rs
@@ -0,0 +1,55 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        let name = &args[0];
+        println!("usage: {name} <file>");
+        return Err("Error: expected file arg".into());
+    }
+
+    let re = regex::Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+
+    let contents = std::fs::read_to_string(&args[1])?;
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+
+    for line in contents.lines() {
+        if line.trim().is_empty() {
+            // Stacks are done, reverse each stack
+            assert!(!stacks.is_empty());
+            for v in &mut stacks {
+                v.reverse();
+            }
+        } else if line.starts_with("move") {
+            let m = re.captures(line).unwrap();
+            let count = m[1].parse::<usize>().unwrap();
+            let start = m[2].parse::<usize>().unwrap() - 1;
+            let end = m[3].parse::<usize>().unwrap() - 1;
+            assert!(start < stacks.len() && end < stacks.len());
+
+            let new_len = stacks[start].len() - count;
+            let tmp: Vec<char> = stacks[start].iter().skip(new_len).cloned().collect();
+            stacks[end].extend(tmp);
+            stacks[start].truncate(new_len);
+        } else {
+            let itr = line.chars().skip(1);
+            for (i, c) in itr.enumerate() {
+                if i % 4 == 0 && ('A'..='Z').contains(&c) {
+                    let idx = i / 4;
+                    if idx >= stacks.len() {
+                        stacks.extend(std::iter::repeat(Vec::new()).take(idx - stacks.len() + 1));
+                    }
+                    stacks[idx].push(c);
+                }
+            }
+        }
+    }
+
+    println!(
+        "Answer: {}",
+        stacks
+            .into_iter()
+            .map(|v| v.last().cloned().unwrap_or(' '))
+            .collect::<String>()
+    );
+
+    Ok(())
+}
